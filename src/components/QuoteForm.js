87572f8fc7f913da534872c7c19426728259cba6
Kaisen Ye <75810017+kaisenye@users.noreply.github.com>
2023-09-12 12:16:15 -0700

    small updates

--- src/components/QuoteForm.js
+++ src/components/QuoteForm.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState }from 'react';
 
 // css
 import '../css/QuoteForm.css';
@@ -19,7 +19,7 @@ const QuoteForm = () => {
         //... add more countries as needed
     ];
 
-    //... inside QuoteForm component
+    // formData inside QuoteForm component
     const [formData, setFormData] = React.useState({
         name: "",
         email: "",
@@ -32,10 +32,37 @@ const QuoteForm = () => {
         details: ""
     });
 
+    // validate form
+    const validateForm = () => {
+        let missing = [];
+        
+        if (!formData.name) missing.push("NAME");
+        if (!formData.email) missing.push("EMAIL");
+        if (!formData.phone) missing.push("PHONE");
+        if (!formData.company) missing.push("COMPANY");
+        if (formData.country === 'Select Country') missing.push("COUNTRY");
+        if (!formData.industry) missing.push("INDUSTRY");
+
+        return missing;
+    };
+    
 
 
+    // handle form submission
+    const [missingFields, setMissingFields] = useState([]); 
+    const [notification, setNotification] = useState(""); 
     const handleSubmit = async (e) => {
         e.preventDefault();
+
+        // validate form
+        const missing = validateForm();
+
+        if (missing.length > 0) {
+            setMissingFields(missing);
+            return;
+        }
+
+        // send email
         try {
             const response = await fetch('http://localhost:3001/send-email', {
                 method: 'POST',
@@ -48,13 +75,15 @@ const QuoteForm = () => {
                 console.log("Email sent successfully!");
             } else {
                 console.log("Error sending email.");
+                setNotification("Error sending email.");
             }
         } catch (error) {
             console.error("There was an error sending the email:", error);
+            setNotification("There was an error sending the email.");
         }
 
         // display success message
-        alert("Thank you for your request! We will get back to you as soon as possible.");
+        setNotification("Thank you for your request! We will get back to you as soon as possible.");
 
         // clear form
         setFormData({
@@ -77,45 +106,40 @@ const QuoteForm = () => {
         <form className='quote-form' onSubmit={handleSubmit}>
             <div className='quote-form-content'>
                 <div className='quote-form-item'>
-                    <label htmlFor="name" className="page-text-1">NAME:</label>
+                    <label htmlFor="name" className="page-text-1">NAME: <span>*</span> </label>
                     <input type="text" 
                             id="name" 
-                            placeholder='Enter Your Full Name' 
-                            required 
+                            placeholder='Enter Your Full Name'  
                             value={formData.name} 
                             onChange={(e) => setFormData(prev => ({ ...prev, name: e.target.value }))}/>
                 </div>
                 <div className='quote-form-item'>
-                    <label htmlFor="email" className="page-text-1">EMAIL:</label>
+                    <label htmlFor="email" className="page-text-1">EMAIL: <span>*</span> </label>
                     <input type="email" 
                             id="email" 
-                            placeholder='Enter Your Email' 
-                            required 
+                            placeholder='Enter Your Email'  
                             value={formData.email} 
                             onChange={(e) => setFormData(prev => ({ ...prev, email: e.target.value }))}/>
                 </div>
                 <div className='quote-form-item'>
-                    <label htmlFor="phone" className="page-text-1">PHONE:</label>
+                    <label htmlFor="phone" className="page-text-1">PHONE: <span>*</span> </label>
                     <input type="phone" 
                             id="phone" 
-                            placeholder='Enter Your Phone Number' 
-                            required 
+                            placeholder='Enter Your Phone Number'  
                             value={formData.phone} 
                             onChange={(e) => setFormData(prev => ({ ...prev, phone: e.target.value }))}/>
                 </div>
                 <div className='quote-form-item'>
-                    <label htmlFor="company" className="page-text-1">COMPANY:</label>
+                    <label htmlFor="company" className="page-text-1">COMPANY: <span>*</span> </label>
                     <input type="text" 
                             id="company" 
-                            placeholder='Enter Your Company Name' 
-                            required 
+                            placeholder='Enter Your Company Name'  
                             value={formData.company} 
                             onChange={(e) => setFormData(prev => ({ ...prev, company: e.target.value }))}/>
                 </div>
                 <div className='quote-form-item'>
-                    <label htmlFor="country" className="page-text-1">COUNTRY:</label>
+                    <label htmlFor="country" className="page-text-1">COUNTRY: <span>*</span> </label>
                     <select id="country" 
-                            required
                             value={formData.country} 
                             onChange={(e) => setFormData(prev => ({ ...prev, country: e.target.value }))}>
                         {countries.map(country => (
@@ -128,25 +152,22 @@ const QuoteForm = () => {
                     <input type="text" 
                             id="address" 
                             placeholder='Enter Your Address' 
-                            required 
                             value={formData.address} 
                             onChange={(e) => setFormData(prev => ({ ...prev, address: e.target.value }))}/>
                 </div>
                 <div className='quote-form-item'>
-                    <label htmlFor="industry" className="page-text-1">INDUSTRY:</label>
+                    <label htmlFor="industry" className="page-text-1">INDUSTRY: <span>*</span> </label>
                     <input type="text" 
                             id="industry" 
-                            placeholder='What industry are you in?' 
-                            required 
+                            placeholder='What industry are you in?'  
                             value={formData.industry} 
                             onChange={(e) => setFormData(prev => ({ ...prev, industry: e.target.value }))}/>
                 </div>
                 <div className='quote-form-item'>
-                    <label htmlFor="product" className="page-text-1">PRODUCT:</label>
+                    <label htmlFor="product" className="page-text-1">PRODUCT: </label>
                     <input type="text" 
                             id="product" 
                             placeholder='Interested in a specific HIPPSC product?' 
-                            required 
                             value={formData.product} 
                             onChange={(e) => setFormData(prev => ({ ...prev, product: e.target.value }))}/>
                 </div>
@@ -161,6 +182,15 @@ const QuoteForm = () => {
                     </textarea>
                 </div>
             </div>
+
+            <div className='quote-form-alert'>
+                {/* You can style this div to make the alert more visually appealing */}
+                {missingFields.length > 0 && `Please fill out the following fields: ${missingFields.join(', ')}`}
+            </div>
+            <div className="quote-form-notification">
+                {notification}
+            </div>
+
             <div className='quote-form-btn'>
                 <button type="submit" className="rectangle-btn">
                     Submit <span className="rectangle-btn-icon">→</span>
