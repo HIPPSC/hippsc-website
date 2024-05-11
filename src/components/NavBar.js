5bdfac1f714799d31a0a0a05cc3335ab35b42121
Kaisen Ye <75810017+kaisenye@users.noreply.github.com>
2024-05-11 14:03:03 -0700

    modify navbar font

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -115,36 +115,36 @@ const NavBar = () => {
 
                 <div className="nav-bar-left">
                     {/* PRODUCTS */}
-                    <div className="nav-bar-item nav-bar-left-item dropdown page-text-2" 
+                    <div className="nav-bar-item nav-bar-left-item dropdown page-text-1" 
                          onClick={handleProductDropdownItemClick}>
                             {t("navbar.p")}
                         <div className={`nav-bar-dropdown-content ${!productDropdownVisible ? 'nav-bar-dropdown-content-hidden' : ''}`} ref={productDropdownRef}>
                             <Link to="/product/shrink-fit-tool-holders" 
-                                className='nav-bar-dropdown-item page-text-2'
+                                className='nav-bar-dropdown-item page-text-1'
                                 onClick={handleProductDropdownItemClick}>
                                     {t("navbar.pS")}
                                     {/* Shrink Fit Holders */}
                             </Link>
                             <Link to='/product/shrink-fit-machines/h6i'
-                                className='nav-bar-dropdown-item page-text-2'
+                                className='nav-bar-dropdown-item page-text-1'
                                 onClick={handleProductDropdownItemClick}>
                                     {t("navbar.pH")}
                                     {/* H6i Shrink Fit Machine */}
                             </Link>
                             <Link to='/product/shrink-fit-machines/f15i'
-                                className='nav-bar-dropdown-item page-text-2'
+                                className='nav-bar-dropdown-item page-text-1'
                                 onClick={handleProductDropdownItemClick}>
                                     {t("navbar.pF")}
                                     {/* F15i Shrink Fit Machine */}
                             </Link>
                             <Link to='/product/tool_presetting'
-                                className='nav-bar-dropdown-item page-text-2'
+                                className='nav-bar-dropdown-item page-text-1'
                                 onClick={handleProductDropdownItemClick}>
                                     {t("navbar.pL")}
                                     {/* Laser Presetting Machine */}
                             </Link>
                             <Link to='/product/dial_indicator'
-                                className='nav-bar-dropdown-item page-text-2'
+                                className='nav-bar-dropdown-item page-text-1'
                                 onClick={handleProductDropdownItemClick}>
                                     {t("navbar.pD")}
                                     {/* Laser Presetting Machine */}
@@ -154,25 +154,25 @@ const NavBar = () => {
 
                     {/* SERVICES */}
                     <Link to="/service"
-                        className="nav-bar-item nav-bar-left-item page-text-2">
+                        className="nav-bar-item nav-bar-left-item page-text-1">
                         {t("navbar.s")}
                     </Link>
 
                     {/* CONTACT */}
                     <Link to="/contact" 
-                          className="nav-bar-item nav-bar-left-item page-text-2">
+                          className="nav-bar-item nav-bar-left-item page-text-1">
                         {t("navbar.c")}
                     </Link>
 
                     {/* ABOUT */}
                     <Link to="/about" 
-                          className="nav-bar-item nav-bar-left-item page-text-2">
+                          className="nav-bar-item nav-bar-left-item page-text-1">
                         {t("navbar.a")}
                     </Link>
 
                     {/* BLOG */}
                     <Link to="/blog" 
-                          className="nav-bar-item nav-bar-left-item page-text-2">
+                          className="nav-bar-item nav-bar-left-item page-text-1">
                         {t("navbar.b")}
                     </Link>
                     
@@ -184,17 +184,17 @@ const NavBar = () => {
                 <div className="nav-bar-right">
                     
                     {/* Language Dropdown */}
-                    <div className="nav-bar-item dropdown page-text-2"
+                    <div className="nav-bar-item dropdown page-text-1"
                         onClick={() => setLgDropdownVisible(!lgDropdownVisible)}>
                             <div className="language-dropdown">
                                 <CiGlobe className='nav-bar-dropdown-icon' />
                                 {t("navbar.l")}
                             </div>
                         <div className={`nav-bar-dropdown-content ${!lgDropdownVisible ? 'nav-bar-dropdown-content-hidden' : ''}`} ref={lgDropdownRef}>
-                            <div className="nav-bar-dropdown-item page-text-2" onClick={() => switchLanguage('en')}>
+                            <div className="nav-bar-dropdown-item page-text-1" onClick={() => switchLanguage('en')}>
                                 English
                             </div>
-                            <div className="nav-bar-dropdown-item page-text-2" onClick={() => switchLanguage('cn')}>
+                            <div className="nav-bar-dropdown-item page-text-1" onClick={() => switchLanguage('cn')}>
                                 中文
                             </div>
                         </div>
